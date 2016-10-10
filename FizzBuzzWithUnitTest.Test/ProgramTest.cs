0f4a956f60f0196f95f1f9756ea3adae2fda189f
oleynikdev <oleynikdev@gmail.com>
2016-10-10 22:00:38 +0300

    The project push

---
+++ FizzBuzzWithUnitTest.Test/ProgramTest.cs
@@ -0,0 +1,63 @@
+﻿using FizzBuzzWithUnitTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzWithUnitTest.Test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void GetResult_Should_Be_Fizz_If_Number_Division_On_3_By_Module()
+        {
+            //arrange
+
+            int i = 3;
+            string expected = "Fizz";
+
+            //act
+
+            string result = Program.GetResult(i);
+
+            //assert
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void GetResult_Should_Be_Buzz_If_Number_Division_On_5_By_Module()
+        {
+            //arrange
+
+            int i = 5;
+            string expected = "Buzz";
+
+            //act
+
+            string result = Program.GetResult(i);
+
+            //assert
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void GetResult_Should_Be_FizzBuzz_If_Number_Division_On_15_By_Module()
+        {
+            //arrange
+
+            int i = 15;
+            string expected = "FizzBuzz";
+
+            //act
+
+            string result = Program.GetResult(i);
+
+            //assert
+
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
---
+++ FizzBuzzWithUnitTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzzWithUnitTests
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Print();
+        }
+
+        public static void Print()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(GetResult(i));
+            }
+        }
+
+        public static string GetResult(int i)
+        {
+            string result = null;
+            
+            if (i % 3 == 0)
+                result = "Fizz";
+
+            if (i % 5 == 0)
+                result += "Buzz";
+            
+            return result ?? i.ToString();
+        }
+    }
+}
